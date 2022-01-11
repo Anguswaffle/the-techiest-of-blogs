daf196c866faa22afcbd2e7f3b39658a084e390c
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2022-01-10 23:14:49 -0800

    making home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const {User, Post, Comment} = require('../models');
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 // Loads homepage
 router.get('/', async (req, res) => {
@@ -13,7 +14,7 @@ router.get('/', async (req, res) => {
       ],
     });
 
-    const posts = postData.map(post => post.get({plain: true}))
+    const posts = postData.map(post => post.get({ plain: true }))
 
     res.render('homepage', {
       posts,
@@ -21,6 +22,61 @@ router.get('/', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+})
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+        {
+          model: Comment,
+          attributes: ['content'],
+        },
+      ]
+    });
+
+    const post = postData.get({plain: true});
+
+    res.render('post', {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password']},
+      include: [{ model: Post }]
+    })
+
+    const user = userData.get({plain: true});
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+router.get('/login', async (req, res) => {
+  // If user is logged in then redirects to profile page
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  //Otherwise login page is rendered
+  res.render('login');
 })
 
 module.exports = router;
