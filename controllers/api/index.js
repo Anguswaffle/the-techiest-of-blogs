85fe2cd2a29753131ff169a39e3b687df4b3f5bc
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2022-01-11 00:38:19 -0800

    Signup added

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,10 @@
-const rotuer = require('express').Router();
+const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentRoutes');
-const { Router } = require('express');
 
-Router.use('/users', userRoutes)
-Router.use('/posts', postRoutes)
-Router.use('/comments', commentRoutes)
+router.use('/users', userRoutes)
+// router.use('/posts', postRoutes)
+// router.use('/comments', commentRoutes)
+
+module.exports = router;
